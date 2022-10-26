e5f25a908a5bde0b4fd759fb79915ab2194aafff
TGOLD <thomaswgold90@gmail.com>
2022-10-25 21:54:39 -0600

    thought virtual

--- models/User.js
+++ models/User.js
@@ -41,6 +41,9 @@ const userSchema = new Schema(
 userSchema.virtual('friendCount').get(function () {
     return this.friends.length;
 });
+userSchema.virtual('thoughtCount').get(function () {
+    return this.thoughts.length;
+});
 
 const User = model('User', userSchema);
 
